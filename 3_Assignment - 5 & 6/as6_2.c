6b994f0a3f302f2d7b5bbba078877cd4c13c4a88
Saikat36 <saikatp360@gmail.com>
2025-09-15 23:35:39 +0530

    added

---
+++ 3_Assignment - 5 & 6/as6_2.c
@@ -0,0 +1,33 @@
+
+
+/* Give me the most optimize solution of this question - */
+
+
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, percent;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+    {
+        percent = ((sp - cp) / cp) * 100;
+        printf("Profit Percentage = %.2f%%\n", percent);
+    }
+    else if (sp < cp)
+    {
+        percent = ((cp - sp) / cp) * 100;
+        printf("Loss Percentage = %.2f%%\n", percent);
+    }
+    else
+    {
+        printf("No Profit, No Loss\n");
+    }
+
+    return 0;
+}
